1fa07a80cbc57fb40ab8b70194739c5dad9fb69f
jayraj1906 <jayrajchaudhary02@gmail.com>
2023-03-09 13:28:59 +0530

    third commit 2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@
 function App() {
 
   function handleClick(){
-    console.log(process.env.NAME)
-    console.log(process.env.EMAIL)
+    console.log(process.env.REACT_APP_NAME)
+    console.log(process.env.REACT_APP_EMAIL)
   }
   return (
     <div className="App">
